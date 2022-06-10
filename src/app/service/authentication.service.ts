c75830c7ed77b9aba587ffd65ddef423de67dc43
Martín <magadiflo@gmail.com>
2022-06-10 12:49:08 -0500

    User list service call

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { JwtHelperService } from "@auth0/angular-jwt";
 
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
+import { User } from '../model/user';
+
+const HOST: string = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${HOST}/user/list`);
+  }
+
 }

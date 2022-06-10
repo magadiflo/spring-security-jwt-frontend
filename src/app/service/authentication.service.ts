e8404aea7a616aaaaf7ada6ee06045bb4536ef7e
Martín <magadiflo@gmail.com>
2022-06-09 19:12:22 -0500

    Login service call

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+const HOST: string = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    //*observe: 'response', significa que HttpClient devolverá toda la respuesta completa, 
+    //*incluyendo encabezados y todo. Esto lo hacemos por que el token que nos devuelve el backend 
+    //*viene en el encabezado, y si no ponemos esa opción por defecto solo nos devolverá el cuerpo de la petición.
+    //*Como resultado, devuelve un Observable de tipo HttpResponse en lugar de solo los datos JSON contenidos en el cuerpo.
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${HOST}/user/login`, user, { observe: 'response' }); 
+  }
+
 }

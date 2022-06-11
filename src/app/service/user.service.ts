57269ce0b3b44fdf86444e82e765eb9f117d5d69
Martín <magadiflo@gmail.com>
2022-06-10 19:08:30 -0500

    Delete user service call

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -40,4 +40,8 @@ export class UserService {
     return this.http.post<User>(`${HOST}/user/update-profile-image`, formData, { reportProgress: true, observe: 'events' });
   }
 
+  deleteUser(userId: number): Observable<any> {
+    return this.http.delete<any>(`${HOST}/user/delete/${userId}`);
+  }
+
 }

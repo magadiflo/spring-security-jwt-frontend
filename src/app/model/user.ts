339b9fc00702b5f0d7077c9a7ae4f07976e316c4
Martín <magadiflo@gmail.com>
2022-06-10 20:03:44 -0500

    Adding user class properties

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,5 +1,18 @@
 export class User {
 
-    
+    constructor(public userId: string = '',
+        public firstName: string = '',
+        public lastName: string = '',
+        public username: string = '',
+        public password: string = '',
+        public email: string = '',
+        public profileImageUrl: string = '',
+        public lastLoginDate: Date | null = null,
+        public lastLoginDateDisplay: Date | null = null,
+        public joinDate: Date | null = null,
+        public role: string = '',
+        public authorities: string[] = [],
+        public isActive: boolean = false,
+        public isNotLocked: boolean = false) { }
 
 }

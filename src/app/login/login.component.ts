4a430015e086ec9ab7e9ff3fd12e6157f893c34f
Martín <magadiflo@gmail.com>
2022-06-14 13:11:56 -0500

    Login component - Part 1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../service/authentication.service';
+import { NotificationService } from '../service/notification.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+    if (this.authenticationService.isUserLoggedIn()) {
+      this.router.navigate(['/user', 'management']);
+    } else {
+      this.router.navigate(['/login']);
+      console.log('abriendo login...');  
+    }
   }
 
+  ngOnDestroy(): void {
+
+  }
 }

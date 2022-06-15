c58d02c91d1bc670aa6dfecd6a964db6f40755eb
Martín <magadiflo@gmail.com>
2022-06-15 12:26:33 -0500

    Adding tab name - Part 1

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  /**
+   ** BehaviorSubject, es un tipo de Observable que emite múltiples valores a uno 
+   ** o muchos Observers. Sus características son:
+   ** - Requiere un valor inicial, por que siempre debe devolver
+   **   un valor en el momento de la subscripción, incluso si no ha recibido el next().
+   ** - Después de subscribirse, devuelve el último valor del tema.
+   ** - Puede utilizar el método getvalue () en cualquier momento para recuperar el último 
+   **   valor del tema en código no observable.
+   ** El signo $, es una convención para declarar un observable
+   */
+  private titleSubject = new BehaviorSubject<string>('Users');
+  titleAction$ = this.titleSubject.asObservable();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeTitle(title: string): void {
+    //*Así cambiamos el valor
+    this.titleSubject.next(title);
   }
 
 }

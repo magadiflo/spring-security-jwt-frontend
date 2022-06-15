7350b194b89556aa6968b843455e09a80582e3ca
Martín <magadiflo@gmail.com>
2022-06-14 23:34:18 -0500

    Test register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { Subscription } from 'rxjs';
 
 import { User } from '../model/user';
+import { AuthenticationService } from '../service/authentication.service';
+import { NotificationService } from '../service/notification.service';
+import { NotificationType } from '../enum/notification-type.enum';
 
 @Component({
   selector: 'app-register',
@@ -14,17 +19,47 @@ export class RegisterComponent implements OnInit, OnDestroy {
   showLoading: boolean = false;
   private subscriptions: Subscription[] = [];
 
-  constructor() { }
-  
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private notificationService: NotificationService) { }
+
   ngOnInit(): void {
+    if (this.authenticationService.isUserLoggedIn()) {
+      this.router.navigate(['/user', 'management']);
+    }
   }
-  
+
   ngOnDestroy(): void {
     this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
   onRegister(user: User): void {
+    console.log(user);
+    this.showLoading = true;
+    const loginSub = this.authenticationService.register(user)
+      .subscribe({
+        next: (resp: User) => {
+          this.showLoading = false;
+          this.sendNotification(NotificationType.SUCCESS, `A new account was created for ${resp.firstName}. 
+          Please check your email for password to log in.`);
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log(err);
+          this.sendNotification(NotificationType.ERROR, err.error.message);
+          this.showLoading = false;
+        },
+        complete: () => console.log('Subscripción register() completa!')
+      });
 
+    this.subscriptions.push(loginSub);
+  }
+
+  private sendNotification(notificationType: NotificationType, message: string): void {
+    if (!message) {
+      message = 'An error ocurred. Please, try again';
+    }
+    this.notificationService.notify(notificationType, message);
   }
 
 }

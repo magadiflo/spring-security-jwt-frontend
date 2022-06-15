4658d71462beb219b014fab62ff08cbfbb6d8b40
Martín <magadiflo@gmail.com>
2022-06-14 23:16:17 -0500

    Register template

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+
+  showLoading: boolean = false;
+  private subscriptions: Subscription[] = [];
 
   constructor() { }
-
+  
   ngOnInit(): void {
+  }
+  
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+  onRegister(user: User): void {
+
   }
 
 }
